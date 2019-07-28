b080c04d5342a2224694a156a883cf83b3711c15
home <orcasuit@gmail.com>
2019-07-29 05:16:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.Session;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private Button btn_custom_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
+
+        btn_custom_login = (Button) findViewById(R.id.btn_custom_login);
+
+        btn_custom_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Session session = Session.getCurrentSession();
+                session.addCallback(new SessionCallback());
+                session.open(AuthType.KAKAO_LOGIN_ALL, LoginActivity.this);
+            }
+
+        });
+
+
+    }
+
+}
